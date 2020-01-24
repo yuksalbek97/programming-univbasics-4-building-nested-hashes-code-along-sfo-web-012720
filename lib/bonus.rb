fe2da439e5fcbe1c457fe40433e5aeb01c8043d0
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 03:13:47 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,8 @@ def bonus
 
   #code your solution here:
   
-epic_tragedy[:montague][:hero][:name] = "dead"
-epic_tragedy[:capulet][:heroine][:name] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   
 
