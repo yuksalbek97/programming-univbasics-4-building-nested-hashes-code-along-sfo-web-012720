152fb1ee9df4e9f28810dda8f61f5c5d70eb6900
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 02:50:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord of Montague", 
+        :name => "Lord Montague", 
         :age => "53"
         },
       :matriarch => {
